9ce18a2c1ea24ccf0d3ae5c1db48a922daf68aca
Ezequiel Bengoechea <eze.bengo224@gmail.com>
2026-01-14 18:19:02 -0300

    Update axiosInstance.ts

--- src/shared/libs/axios/axiosInstance.ts
+++ src/shared/libs/axios/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://backofficebacktesting.tgdcompany.com/",
+  baseURL: "http://testback.tgdcompany.com/",
   headers: {
     "Content-Type": "application/json",
   },
